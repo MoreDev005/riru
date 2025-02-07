f97ad46072ea8b3683c2700c630817efa5b0a00a
MoreDev005 <bottoramku@gmail.com>
2025-02-07 15:38:37 +0700

    Update online.js

--- online.js
+++ online.js
@@ -3,7 +3,7 @@ const path = require("path");
 const express = require("express");
 const app = express()
 var http = require('http').Server(app);
-const port = process.env.SERVER_PORT
+const port = 3000//process.env.SERVER_PORT
 const browserpath = path.join(__dirname, "");
 
 app.use(express.static(browserpath));
